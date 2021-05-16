93038a1f5c3e08b3a716eb72c1eb1fe74ac9a0c8
Dan Sullivan <dansullivan@gmail.com>
2021-05-16 14:46:05 -0400

    monitor devopsrockstars

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,39 @@ provider "newrelic" {
   api_key    = var.new_relic_api_key
   region     = "US"
 }
+
+resource "newrelic_alert_channel" "slack_channel" {
+  name = "slack-terraform"
+  type = "slack"
+
+  config {
+    url     = var.slack_webhook_url
+    channel = "notifications"
+  }
+}
+
+resource "newrelic_alert_policy" "policy_with_channels" {
+  name                = "terraform-slack-channel"
+  incident_preference = "PER_CONDITION"
+
+  channel_ids = [
+    newrelic_alert_channel.slack_channel.id,
+  ]
+}
+
+resource "newrelic_synthetics_monitor" "devopsrockstars" {
+  name       = "devopsrockstars-terraform"
+  type       = "SIMPLE"
+  frequency  = 1
+  status     = "ENABLED"
+  locations  = ["AWS_US_EAST_1"]
+  uri        = "https://devopsrockstars.com"
+  verify_ssl = true
+}
+
+resource "newrelic_synthetics_alert_condition" "devopsrockstars" {
+  policy_id = newrelic_alert_policy.policy_with_channels.id
+
+  name       = "devopsrockstars-terraform"
+  monitor_id = newrelic_synthetics_monitor.devopsrockstars.id
+}
