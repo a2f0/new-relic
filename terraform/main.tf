2a43e4618c3b8172ae008b70f0becee0884441f2
Dan Sullivan <dansullivan@gmail.com>
2021-05-16 14:47:30 -0400

    move devopsrockstars into separate file

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,20 +32,3 @@ resource "newrelic_alert_policy" "policy_with_channels" {
     newrelic_alert_channel.slack_channel.id,
   ]
 }
-
-resource "newrelic_synthetics_monitor" "devopsrockstars" {
-  name       = "devopsrockstars-terraform"
-  type       = "SIMPLE"
-  frequency  = 1
-  status     = "ENABLED"
-  locations  = ["AWS_US_EAST_1"]
-  uri        = "https://devopsrockstars.com"
-  verify_ssl = true
-}
-
-resource "newrelic_synthetics_alert_condition" "devopsrockstars" {
-  policy_id = newrelic_alert_policy.policy_with_channels.id
-
-  name       = "devopsrockstars-terraform"
-  monitor_id = newrelic_synthetics_monitor.devopsrockstars.id
-}
